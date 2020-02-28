8fa991a2048f9497f43c673f6323c704670497ec
Genard Ivan Tejano <genardivanteajno@Genards-MacBook-Pro.local>
2020-02-28 08:21:50 -0800

    cloone

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import Header from "./components/Header.js";
+import CharacterList from "./components/CharacterList";
+import CharacterCard from "./components/CharacterCard";
+import WelcomePage from "./components/WelcomePage.js";
+
 
 
 export default function App() {
   return (
     <main>
       <Header />
+      <WelcomePage />
     </main>
   );
 }
