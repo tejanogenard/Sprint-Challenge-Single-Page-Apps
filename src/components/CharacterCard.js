26e9888b8dd3b15db0625fadc818c7143929167d
Genard Ivan Tejano <genardivanteajno@Genards-MacBook-Pro.local>
2020-02-28 12:57:45 -0800

    save

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { Card, CardBody, Button, CardTitle, CardText,
   CardImg, Col } from 'reactstrap';
+  import { Link } from 'react-router-dom'; 
 
 export default function CharacterCard(props) {
   return(
     <div>
+              <Col xs = '10' md ='10' xl = '10'>
+                <Link to = {`/CharacterCard/${props.name}`}>
           <Card>
           <CardImg top width="100%" src={props.image} alt="Card image cap" />
         <CardBody>
@@ -15,6 +18,8 @@ export default function CharacterCard(props) {
           </CardText>
         </CardBody>
           </Card>
+          </Link>
+          </Col>
     </div>
   )
 }
