8fcf37d4eb0c9e004713a1c9eaf2371926941bf9
Genard Ivan Tejano <genardivanteajno@Genards-MacBook-Pro.local>
2020-02-28 08:57:46 -0800

    update characterList

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ export default function App() {
     <main>
       <Header />
       <WelcomePage />
+      <CharacterList />
     </main>
   );
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,35 @@
 import React, { useEffect, useState } from "react";
+import axios from 'axios';
+import characterCard from './CharacterCard';
+import CharacterCard from "./CharacterCard";
+
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+    const[page, setPage] = useState(1)
+    const [data, setData] = useState([])
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
+      axios.get(`https://rickandmortyapi.com/api/character/?page=${page}`)
+      .then(Response => setData(Response.data.results))
+      console.log(Response)
+
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, []);
+  }, [page]);
 
   return (
-    <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
-    </section>
+      <div>
+        {data.map((char,index) => (
+          <CharacterCard 
+          name = {char.name} 
+          status = {char.status} 
+          gender = {char.gender}
+          image = {char.image}
+          key = {index}
+          />
+        ))}
+       
+      </div>
   );
 }
